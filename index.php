e871f75c5876076310d30777f9f6b6e097a22785
eldinh <eldinh>
2016-11-17 16:55:31 +0000

    Initial commits

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta http-equiv="refresh" content="5; url=http://tinvasong.tk/">
+    </head>
+    <body style="background-color: #5CB3FF; color: #C24641; font-weight: bolder;">
+        <div style="width: 700px; margin: auto; margin-top: 50px; border: solid 2px gray; text-align: center; padding: 3px;">
+            <h1>THÔNG BÁO: Kể từ ngày 1 tháng 11 năm 2016, tên miền tinvasong.com đã được đổi thành <a href="//tinvasong.tk">tinvasong.tk</a>.</h1>
+            <br />
+            <h2>Bạn sẽ được tự động chuyển qua trang mới sau 5 giây.</h2>
+        </div>
+    </body>
+</html>
